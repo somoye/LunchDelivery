d6db3efd7123f24ba8deb4a084ef38a9fb3be291
Lika <citrus@iMac-Citrus.local>
2016-09-26 09:46:45 +0300

    Menu-page-update
    Layout are changed.
    Data for menu is taken from server.

--- Server/app.js
+++ Server/app.js
@@ -19,27 +19,39 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 app.get('/menu', cors({
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-}), function(req, res, next){
-  res.json({data: [
-      {name:"Dish", description:"This is tasty", price:120},
-      {name:"Dish", description:"This is tasty", price:120}
-  ]});
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}), function (req, res, next) {
+    res.json({
+        data: [
+            {
+                name: "ФО БО",
+                description: "Ароматный бульон с рисовой лапшой и мясом 555г",
+                price: "60.00"
+            },
+            {
+                name: "Окрошка с RoasBeef",
+                description: "400г",
+                price: "48.00"
+            }
+  ]
+    });
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -47,23 +59,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
