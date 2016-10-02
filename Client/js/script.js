0dd6512de6cc45ae7f33ad98acfac72352280762
Lika <citrus@iMac-Citrus.local>
2016-10-02 21:40:43 +0300

    dynamic menu list generation was added

---
+++ Client/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var list = $('.list-menu-content');
+
+    $.ajax({
+        url: "http://localhost:3001/menu",
+        success: function (response) {
+
+            $.each(response.data, function () {
+                $('<li class="list-product list-product-active"/>').html('<div class="list-product-title"/>' + '<div class="list-product-desc"/>' + '<div class="list-product-price"/>' + '<div class="clear" />' + '<span class="order-product">заказать</span>').appendTo(list)
+            });
+            for (var i = 0; i < response.data.length; i++) {
+
+                $(".list-product-title").eq(i).html(
+                    '<span class="dish">' + response.data[i].name + '</span>' +
+                    '<span class="dotted"></span>'
+                );
+                $(".list-product-desc").eq(i).html('<p class="description">' + response.data[i].description + '</p>');
+                $(".list-product-price").eq(i).html('<span class="price">' + response.data[i].price + '</span>');
+
+            }
+        }
+    });
+
+});
