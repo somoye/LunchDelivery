3158f7e3b8cd3f8d0f3a2d9fcb16968d935fce07
Angelika Liubchenko <angelika.somoye@gmail.com>
2016-10-03 20:29:39 +0300

    jquery installed removed "for" cycle in script.js

--- Client/js/script.js
+++ Client/js/script.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
         url: "http://localhost:3001/menu",
         success: function (response) {
 
-            $.each(response.data, function () {
-                $('<li class="list-product list-product-active"/>').html('<div class="list-product-title"/>' + '<div class="list-product-desc"/>' + '<div class="list-product-price"/>' + '<div class="clear" />' + '<span class="order-product">заказать</span>').appendTo(list)
-            });
-            for (var i = 0; i < response.data.length; i++) {
+            $.each(response.data, function (i) {
+                $('<li class="list-product list-product-active"/>')
+                    .html('<div class="list-product-title"/>' + '<div class="list-product-desc"/>' + '<div class="list-product-price"/>' + '<div class="clear" />' + '<span class="order-product">заказать</span>').appendTo(list)
+
 
                 $(".list-product-title").eq(i).html(
                     '<span class="dish">' + response.data[i].name + '</span>' +
@@ -17,7 +17,7 @@ $(document).ready(function () {
                 $(".list-product-desc").eq(i).html('<p class="description">' + response.data[i].description + '</p>');
                 $(".list-product-price").eq(i).html('<span class="price">' + response.data[i].price + '</span>');
 
-            }
+            });
         }
     });
 
