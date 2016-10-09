ba63561192e10775767c71866121e4898adf4230
Lika <citrus@iMac-Citrus.local>
2016-10-09 22:34:24 +0300

    intermediate work with categories

--- Client/js/script.js
+++ Client/js/script.js
@@ -1,22 +1,47 @@
 $(document).ready(function () {
-    var list = $('.list-menu-content');
+
+    var panel_grid = $('.active-menu');
 
     $.ajax({
         url: "http://localhost:3001/menu",
         success: function (response) {
+            var category = $(response.categories).filter(function (i, n) {
+                return n.name === 'soup'
+            });
 
-            $.each(response.data, function (i) {
-                $('<li class="list-product list-product-active"/>')
-                    .html('<div class="list-product-title"/>' + '<div class="list-product-desc"/>' + '<div class="list-product-price"/>' + '<div class="clear" />' + '<span class="order-product">заказать</span>').appendTo(list)
+            $.each(response.categories, function (i) {
+
+                $('<div class="panel-grid-cell">').html(
+                    '<div class="list-menu">' +
+                    '<h3>' + category[i] + '</h3>' + '</div>' +
+
+                    ' <ul class="list-menu-content"/>').appendTo(panel_grid);
+                $('.list-menu-content').addClass(category[i].name);
+
+            });
+            var list_soup = $('.soup');
 
 
-                $(".list-product-title").eq(i).html(
-                    '<span class="dish">' + response.data[i].name + '</span>' +
-                    '<span class="dotted"></span>'
-                );
-                $(".list-product-desc").eq(i).html('<p class="description">' + response.data[i].description + '</p>');
-                $(".list-product-price").eq(i).html('<span class="price">' + response.data[i].price + '</span>');
 
+            $.each(category, function (j) {
+
+                $.each(category[j].menu, function (i) {
+                    $('<li class="list-product list-product-active"/>')
+                        .html('<div class="list-product-title"/>' +
+                            '<div class="list-product-desc"/>' +
+                            '<div class="list-product-price"/>' +
+                            '<div class="clear" />' +
+                            '<span class="order-product">заказать</span>').appendTo(list_soup);
+
+
+                    $(".list-product-title").eq(i).html(
+                        '<span class="dish">' + category[j].menu[i].name + '</span>' +
+                        '<span class="dotted"></span>'
+                    );
+                    $(".list-product-desc").eq(i).html('<p class="description">' + category[j].menu[i].description + '</p>');
+                    $(".list-product-price").eq(i).html('<span class="price">' + category[j].menu[i].price + '</span>');
+
+                });
             });
         }
     });
--- Server/services/menu.provider.js
+++ Server/services/menu.provider.js
@@ -1,18 +1,76 @@
 module.exports = {
     get: function () {
         return {
-            data: [
+            categories: [
                 {
-                    name: "ФО БО",
-                    description: "Ароматный бульон с рисовой лапшой <<Doshirak>> и мясом 555г",
-                    price: "60.00"
-                },
-                {
-                    name: "Окрошка с RoasBeef",
-                    description: "400г",
-                    price: "48.00"
+                    name: "soup",
+                    image: "\img\bg-food.jpg",
+                    menu: [
+                        {
+                            name: "Английский крем-суп",
+                            description: "с сырными крутонами",
+                            price: "55.00"
+                        },
+                        {
+                            name: "Борщ с телятиной",
+                            description: "с салом собственного копчения на луковом хлебе",
+                            price: "48.00"
+                        },
+                        {
+                            name: "Окрошка со слабосоленым лососем		",
+                            description: "400г",
+                            price: "60.00"
+                        },
+                        {
+                            name: "Окрошка с RoasBeef",
+                            description: "400г",
+                            price: "48.00"
+                        }
+                    ]
+
+                }, {
+                    name: "burger",
+                    image: "\img\bg-food.jpg",
+                    menu: [
+                        {
+                            name: "Burger с фалафелем и харриса",
+                            description: "250/100/50г",
+                            price: "98.00"
+                        },
+                        {
+                            name: "Cheese-burger «Тетчер»",
+                            description: "из телятины и бекона 250/100/50г",
+                            price: "98.00"
+                        },
+                        {
+                            name: "Burger с индейкой и ананасом",
+                            description: "250/100/50г",
+                            price: "98.00"
+                        }
+                    ]
+                }, {
+                    name: "salad",
+                    image: "\img\bg-food.jpg",
+                    menu: [
+                        {
+                            name: "Салат «Греческий»",
+                            description: "c сыром фета и оливками Kalamata",
+                            price: "75.00"
+                        },
+                        {
+                            name: "Салат с креветками",
+                            description: "c рукколой и сыром Филадельфия",
+                            price: "135.00"
+                        },
+                        {
+                            name: "Салат «Цезарь»",
+                            description: "с цыпленком, жареным на углях",
+                            price: "75.00"
+                        }
+                    ]
                 }
             ]
+
         };
     }
 }
