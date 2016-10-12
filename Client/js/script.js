7215507d67fe8ef5d9722fee1b347261e36f53df
Angelika Liubchenko <angelika.somoye@gmail.com>
2016-10-12 22:13:43 +0300

    menu generation depending on the content is finished

--- Client/js/script.js
+++ Client/js/script.js
@@ -5,44 +5,31 @@ $(document).ready(function () {
     $.ajax({
         url: "http://localhost:3001/menu",
         success: function (response) {
-            var category = $(response.categories).filter(function (i, n) {
-                return n.name === 'soup'
-            });
+
 
             $.each(response.categories, function (i) {
 
                 $('<div class="panel-grid-cell">').html(
                     '<div class="list-menu">' +
-                    '<h3>' + category[i] + '</h3>' + '</div>' +
+                    '<h3>' + response.categories[i].name + '</h3>' + '</div>' +
+                    ' <ul class="list-menu-content ' + response.categories[i].name + '"/>').appendTo(panel_grid);
 
-                    ' <ul class="list-menu-content"/>').appendTo(panel_grid);
-                $('.list-menu-content').addClass(category[i].name);
-
-            });
-            var list_soup = $('.soup');
-
+                var list = $('.' + response.categories[i].name);
+                $.each(response.categories[i].menu, function (j) {
+                    console.log(list);
 
 
-            $.each(category, function (j) {
-
-                $.each(category[j].menu, function (i) {
                     $('<li class="list-product list-product-active"/>')
-                        .html('<div class="list-product-title"/>' +
-                            '<div class="list-product-desc"/>' +
-                            '<div class="list-product-price"/>' +
+                        .html('<div class="list-product-title">' + '<span class="dish">' + response.categories[i].menu[j].name + '</span>' + '<span class="dotted"></span>' + '</div>' +
+                            '<div class="list-product-desc">' + '<p class="description">' + response.categories[i].menu[j].description + '</p>' + '</div>' +
+                            '<div class="list-product-price">' + '<span class="price">' + response.categories[i].menu[j].price + '</span>' + '</div>' +
                             '<div class="clear" />' +
-                            '<span class="order-product">заказать</span>').appendTo(list_soup);
+                            '<span class="order-product">заказать</span>').appendTo(list);
 
-
-                    $(".list-product-title").eq(i).html(
-                        '<span class="dish">' + category[j].menu[i].name + '</span>' +
-                        '<span class="dotted"></span>'
-                    );
-                    $(".list-product-desc").eq(i).html('<p class="description">' + category[j].menu[i].description + '</p>');
-                    $(".list-product-price").eq(i).html('<span class="price">' + category[j].menu[i].price + '</span>');
 
                 });
             });
+
         }
     });
 
