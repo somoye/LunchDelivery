b227d846285e3b1ad4f4434b9343265cc4275110
Kirill Antonov <chapluck@gmail.com>
2016-11-16 23:51:25 +0200

    Image file uploading

--- Server/routes/images.route.js
+++ Server/routes/images.route.js
@@ -1,13 +1,21 @@
 var express = require('express');
-var router = express.Router({mergeParams: true});
+var router = express.Router({ mergeParams: true });
 const imageProvider = require('../services/image.provider');
-//var multer = require('multer')
-//var upload = multer({ limits: { fileSize: 5000000 } });
+var multer = require('multer')
+var upload = multer({ limits: { fileSize: 5000000 } });
 
 router.get('/:imageId', function (req, res, next) {
-  imageProvider.get(req.params.imageId, req.params.imageContext)
-    .then(result => res.type("jpeg") && res.send(result.content))
-    .catch(next);
+  if (req.params.imageId == 'new')
+    res.type("jpeg") && res.end(new Buffer(req.session['image'].buffer));
+  else
+    imageProvider.get(req.params.imageId, req.params.imageContext)
+      .then(result => res.type("jpeg") && res.send(result.content))
+      .catch(next);
+});
+
+router.post('/', upload.single('image'), function (req, res, next) {
+    req.session['image'] = req.file;
+    res.json({});
 });
 
 module.exports = router;
