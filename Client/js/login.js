708d1fe308fd4fcd1b8c6792b2a6cdf0823b2844
alexander liubchenko <alexander.liubchenko0907@gmail.com>
2017-07-26 11:22:38 +0300

    Bug#16 Hide button confirm your order when user isn't logged in

--- Client/js/login.js
+++ Client/js/login.js
@@ -4,6 +4,7 @@ function getUserData() {
     makeGetReq("users/me", {}, function (response) {
         $(".user-profile").show();
         $(".login").hide();
+        $("#myBtn").css({"display": "inline-block"}).show();
         getCurrentUserOrders();
         $(".user-name").html("Hi " + response.name + "!");
     }, function () {
@@ -38,6 +39,7 @@ $(".signin").click(function () {
 $(".logout").click(function () {
     $(".login").show();
     $(".user-profile").hide();
+    $("#myBtn").hide();
     makeGetReq("logout/", {}, function (data) {
         console.log("Log out")
 
