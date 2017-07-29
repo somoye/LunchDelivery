d652612d62e80816ac6a9f2c5ce757358855e498
Kirill Antonov <chapluck@gmail.com>
2017-07-30 01:13:48 +0300

    #11 Resolved. Add API to post and get images.

--- Server/routes/images.route.js
+++ Server/routes/images.route.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router({ mergeParams: true });
 const imageProvider = require('../services/image.provider');
-var multer = require('multer')
+var multer = require('multer');
 var upload = multer({ limits: { fileSize: 5000000 } });
 
 router.get('/:imageId', function (req, res, next) {
@@ -13,9 +13,15 @@ router.get('/:imageId', function (req, res, next) {
       .catch(next);
 });
 
-router.post('/', upload.single('image'), function (req, res, next) {
-    req.session['image' + req.params.imageContext] = req.file;
-    res.json({ name: 'image' + req.params.imageContext});
-});
+router.post('/', upload.single('image'), postImage);
+router.post('/:imageId', upload.single('image'), postImage);
+
+function postImage(req, res, next) {
+  imageContext = req.params.imageContext + '_' + (req.params.imageId || 'default');
+  imageProvider.add({context: imageContext, content: new Buffer(req.file.buffer)})
+    .then(result => res.json({ name: 'image' + req.params.imageContext }))
+    .catch(next);
+  //res.type("jpeg") && res.send(new Buffer(req.file.buffer));
+}
 
 module.exports = router;
