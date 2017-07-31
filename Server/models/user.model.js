efcc174853dcb2d35e2c46f90c8e6dcdf4d95df2
Kirill Antonov <chapluck@gmail.com>
2017-08-01 01:22:09 +0300

    #10 Add admin user admin@gmail.com with pwd admin, and flag isAdmin set to true

--- Server/models/user.model.js
+++ Server/models/user.model.js
@@ -3,7 +3,8 @@ module.exports = function (sequelize, DataTypes) {
 	var User = sequelize.define('User', {
 		name: DataTypes.STRING,
 		lastName: DataTypes.STRING,
-		email: DataTypes.STRING
+		email: DataTypes.STRING,
+		isAdmin: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
 	}, {
 		freezeTableName: true,
 		classMethods: {
--- Server/seeders/seeds/user.seed.js
+++ Server/seeders/seeds/user.seed.js
@@ -4,6 +4,12 @@ module.exports = {
 			name: "Ivan",
 			lastName: "Dorn",
 			email: "ivan@dev-pro.net"
+		},
+		{
+			name: "admin",
+			lastName: "admin",
+			email: "admin@gmail.com",
+			isAdmin: true
 		}
 	]
 };
