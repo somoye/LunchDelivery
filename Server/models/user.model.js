712e9c12ad3ff98b49c7c2f614e9fbf67378f53f
paperplane90 <b.rebrov@gmail.com>
2017-08-09 19:14:58 +0300

    Add unique constraint to User's email field #29
    Change category.model definition to add the constraint on DB level

--- Server/models/user.model.js
+++ Server/models/user.model.js
@@ -1,15 +1,15 @@
 'use strict';
 module.exports = function (sequelize, DataTypes) {
 	var User = sequelize.define('User', {
-		name: { type: DataTypes.STRING, 
+		name: { type: DataTypes.STRING(20), 
 				allowNull: false,
-		 validate: { min: 2, max:20, is: ["^[a-z]+$",'i'] }},
+		 validate: { min: 2, max:20, isAlpha: true }},
 
-		lastName: { type: DataTypes.STRING, 
+		lastName: { type: DataTypes.STRING(20), 
 			allowNull: false,
-		 validate: { min: 2, max:20, is: ["^[a-z]+$",'i'] }},
+		 validate: { min: 2, max:20, isAlpha: true }},
 
-		email:{ type: DataTypes.STRING, 
+		email:{ type: DataTypes.STRING(50), 
 			allowNull: false,			 
 			 validate: {len: [2,50], isEmail: true}			},
 			
