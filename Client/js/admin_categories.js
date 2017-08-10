9fe57be822ffe3b5a211ce6fc9729ed36aaa8c48
Lika <angelika.somoye@gmail.com>
2017-08-10 17:58:36 +0300

    #21 Add an ability to delete category

--- Client/js/admin_categories.js
+++ Client/js/admin_categories.js
@@ -7,12 +7,21 @@ function getCategorisList() {
             '<td>' + sortedList[i].id + '</td>' +
             '<td>' + sortedList[i].name + '</td>' +
             '<td>' + sortedList[i].imageUrl + '</td>' +
+            '<td>' + '<a href="#" class="btn btn-danger delete-category">Delete</a>' + '</td>' +
             '</tr>'
             ).appendTo('.categories-table tbody');
+
+            var categoryId = sortedList[i].id;
+            $(".delete-category").click(function() {
+                deleteCategory(categoryId) 
+            })
         })
-        
-        console.log(sortedList)
+        console.log(response);
     })
+};
+
+function deleteCategory(categoryId) {
+    makeDeleteReq("categories/" + categoryId, getCategorisList());
 };
 
 getCategorisList();
